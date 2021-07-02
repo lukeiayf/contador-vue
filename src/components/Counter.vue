09596be0d6335992f049aaa74fe54864841877ce
Lucas Gomes <79646552+lukeiayf@users.noreply.github.com>
2021-07-02 15:27:24 -0300

    Add files via upload

---
+++ src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <p>Initial Value: {{initialValue}}</p>
+  <p>Actual Value: {{count}}</p>
+  <button @click="add">Add</button>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  name: 'Counter',
+  props: {
+    initialValue: Number
+  },
+  setup(props){
+    console.log(props);
+
+    const count= ref(props.initialValue);
+
+    const add = () => {
+      count.value++;
+    }
+
+    return {count, add}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
